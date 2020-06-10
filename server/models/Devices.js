15074671e232b6ec53aaa46cbac57c6ef1ff2c99
Dat Nguyen <nguyentamdat@gmail.com>
2020-06-10 14:13:59 +0700

    fix type value in log

--- server/models/Devices.js
+++ server/models/Devices.js
@@ -12,7 +12,7 @@ const DeviceSchema = new mongoose.Schema(
         configTopic: { type: String, required: true },
         listenTopic: { type: String, required: true },
         id: { type: String, unique: true, required: true },
-        log: [{ value: String, time: { type: Date, default: Date.now } }],
+        log: [{ value: [String], time: { type: Date, default: Date.now } }],
     },
     { strict: false }
 );
