6573be0444fa3ab2b615a33fe372a6de2a76c91c
Dat Nguyen <nguyentamdat@gmail.com>
2020-06-28 15:08:08 +0700

    fix over heap size

--- server/helper/mqtt.js
+++ server/helper/mqtt.js
@@ -24,49 +24,70 @@ client.subscribe("Topic/#", { qos: 2 }, (err, granted) => {
 });
 
 setInterval(() => {
-    Devices.find((err, res) => {
-        const checkInRange = (val, min, max) => val >= min && val <= max;
-        res = res.map((item) => ({
-            id: item.id,
-            min: item.min,
-            max: item.max,
-            latest: item.log[item.log.length - 1].values.map((x) =>
-                parseInt(x)
-            ),
-        }));
-        res = res
-            .filter((item) => item.min.length > 0 && item.max.length > 0)
-            .map((item) => [item.min, item.max, item.latest]);
-        res = res.map((item) =>
-            item[0].map((_, col) => item.map((x) => x[col]))
-        );
-        res = res.reduce((prev, curr) => prev.concat(curr), []);
-        res = res.map((x) => checkInRange(x[2], x[0], x[1]));
-        res = res.every((x) => x);
-        if (!res) {
-            client.publish(
-                "Topic/Speaker",
-                JSON.stringify([
-                    { device_id: "Speaker", values: ["1", "3000"] },
-                ])
+    Devices.aggregate(
+        [
+            {
+                $project: {
+                    min: "$min",
+                    max: "$max",
+                    id: "$id",
+                    log: { $slice: ["$log", -1] },
+                },
+            },
+        ],
+        (err, res) => {
+            const checkInRange = (val, min, max) => val >= min && val <= max;
+            res = res.map((item) => ({
+                id: item.id,
+                min: item.min,
+                max: item.max,
+                latest: item.log[0].values.map((x) => parseInt(x)),
+            }));
+            res = res
+                .filter(
+                    (item) =>
+                        item.min &&
+                        item.max &&
+                        item.min.length > 0 &&
+                        item.max.length > 0
+                )
+                .map((item) => [item.min, item.max, item.latest]);
+            res = res.map((item) =>
+                item[0].map((_, col) => item.map((x) => x[col]))
             );
-            client.publish(
-                "Topic/LightD",
-                JSON.stringify([{ device_id: "LightD", values: ["1", "255"] }])
-            );
-        } else {
-            client.publish(
-                "Topic/Speaker",
-                JSON.stringify([
-                    { device_id: "Speaker", values: ["0", "3000"] },
-                ])
-            );
-            client.publish(
-                "Topic/LightD",
-                JSON.stringify([{ device_id: "LightD", values: ["0", "255"] }])
-            );
+            res = res.reduce((prev, curr) => prev.concat(curr), []);
+            res = res.map((x) => checkInRange(x[2], x[0], x[1]));
+            console.log(res);
+            res = res.every((x) => x);
+            if (!res) {
+                client.publish(
+                    "Topic/Speaker",
+                    JSON.stringify([
+                        { device_id: "Speaker", values: ["1", "3000"] },
+                    ])
+                );
+                client.publish(
+                    "Topic/LightD",
+                    JSON.stringify([
+                        { device_id: "LightD", values: ["1", "255"] },
+                    ])
+                );
+            } else {
+                client.publish(
+                    "Topic/Speaker",
+                    JSON.stringify([
+                        { device_id: "Speaker", values: ["0", "3000"] },
+                    ])
+                );
+                client.publish(
+                    "Topic/LightD",
+                    JSON.stringify([
+                        { device_id: "LightD", values: ["0", "255"] },
+                    ])
+                );
+            }
         }
-    });
+    );
 }, 5000);
 
 client.on("message", (topic, message, packet) => {
